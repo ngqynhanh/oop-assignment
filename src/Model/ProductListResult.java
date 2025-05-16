4bf3e3d4174ff21234cc6d1ad9bf2f205534282e
Nguyen Quynh Anh <nguyenanh.mywork@gmail.com>
2025-05-17 00:43:15 +0700

    ProductListResult created

---
+++ src/Model/ProductListResult.java
@@ -0,0 +1,28 @@
+package Model;
+
+import java.util.List;
+
+public class ProductListResult {
+    private List<Product> products;
+    private int currentPage;
+    private int totalPages;
+
+    public ProductListResult(List<Product> products, int currentPage, int totalPages) {
+        this.products = products;
+        this.currentPage = currentPage;
+        this.totalPages = totalPages;
+    }
+
+    public List<Product> getOrders() {
+        return products;
+    }
+
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+}
