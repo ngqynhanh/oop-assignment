ebfb7e886936448e03c2274869989ab062a9884d
Nguyen Quynh Anh <nguyenanh.mywork@gmail.com>
2025-05-17 00:43:39 +0700

    OrderListResult created

---
+++ src/Model/OrderListResult.java
@@ -0,0 +1,33 @@
+package Model;
+
+import DBUtil.Implementation;
+import DBUtil.OrderDB;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class OrderListResult {
+    private List<Order> orders;
+    private int currentPage;
+    private int totalPages;
+
+    public OrderListResult(List<Order> orders, int currentPage, int totalPages) {
+        this.orders = orders;
+        this.currentPage = currentPage;
+        this.totalPages = totalPages;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+}
